46e8bdecb2b70da2bda0cb63e49df37b43e84cf1
Rodion Sviridov <sviridov.rod@yandex.ru>
2023-05-14 15:32:14 +0300

    Dockerfile added DB host changed

--- App/src/main/kotlin/com/rodyapal/config/Config.kt
+++ App/src/main/kotlin/com/rodyapal/config/Config.kt
@@ -5,7 +5,7 @@ object Config {
 	const val DATABASE_USER_NAME = "appUser"
 	const val DATABASE_PASSWORD = "appPassword"
 	const val DATABASE_PORT = "5432"
-	const val DATABASE_HOST = "localhost"
+	const val DATABASE_HOST = "postgres"
 	const val DATABASE_DRIVER = "org.postgresql.Driver"
 	const val DATABASE_URL = "jdbc:postgresql://$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME"
 
